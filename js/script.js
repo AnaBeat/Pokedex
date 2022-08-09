7a95050132dd58b3c7623921df27941dc6838388
AnaBeat <abeatrizbcosta@gmail.com>
2022-08-09 20:57:46 -0300

    Feature - Cria botões prev e next

--- js/script.js
+++ js/script.js
@@ -4,10 +4,13 @@ const pokemonImage = document.querySelector('.pokemon__image');
 
 const form = document.querySelector('.form');
 const input = document.querySelector('.input__search');
+const buttonPrev = document.querySelector('.btn-prev');
+const buttonNext = document.querySelector('.btn-next');
 
+let searchPokemon = 1;
 
 const fetchPokemon = async (pokemon) => {
-    const APIResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon.toLowerCase()}`);
+    const APIResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
     
     if (APIResponse.status == 200) {
         const data = await APIResponse.json();
@@ -28,8 +31,10 @@ const renderPokemon = async (pokemon) => {
         pokemonNumber.innerHTML = data.id;
         pokemonImage.src = data['sprites']['versions']['generation-v']['black-white']['animated']['front_default'];
     
-        input.value = '';    
+        input.value = '';
+        searchPokemon = data.id;    
     } else {
+        pokemonImage.style.display = 'none';
         pokemonName.innerHTML = 'Not found :(';
         pokemonNumber.innerHTML = '';
     }
@@ -39,6 +44,20 @@ const renderPokemon = async (pokemon) => {
 form.addEventListener('submit', (event) => {
     event.preventDefault();
 
-    renderPokemon(input.value);
+    renderPokemon(input.value.toLowerCase());
     
 });
+
+buttonPrev.addEventListener('click', () => {
+    if (searchPokemon > 1) {
+        searchPokemon --;
+        renderPokemon(searchPokemon);
+      }
+});
+
+buttonNext.addEventListener('click', () => {
+    searchPokemon ++;
+    renderPokemon(searchPokemon);
+});
+
+renderPokemon(searchPokemon);
